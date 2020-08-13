f53d675ae0702cfbc0d5ae7fea5a6b3b57ec6548
Arter Tendean <arter.tendean.07@gmail.com>
2020-08-13 23:30:59 +0800

    Set Interval For Updating Special Player List

--- dist/ba_data/python/bastd/actor/update_special_player.py
+++ dist/ba_data/python/bastd/actor/update_special_player.py
@@ -1,15 +1,16 @@
-# Run this script with crontab to update special_player list automatically
+# Run this script with crontab, etc to update special_player list automatically
 
 import os
 import requests
 import json
+import time
 
 def main():
 	dir_path = os.path.dirname(os.path.realpath(__file__))
 
 	admin_list = json.loads(requests.get('https://bombsquad.my.id/stats/get_admin/').text)['admin_list']
 
-	
+
 	with open(f'{dir_path}/special_player.py') as file:
 		s = [row for row in file]
 		s[0] = 'admin_list = ' + str(admin_list) + '\n'
@@ -20,4 +21,6 @@ def main():
 		f.close()
 
 if __name__ == '__main__':
-	main()
+	while True:
+		time.sleep(600)
+		main()
